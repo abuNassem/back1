a325f7ec7e13af1e5c9bf9930555546d35caf2d0
abuNaseem <qutaibah0mohmmed@gmail.com>
2025-08-21 19:13:29 +0100

    f

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const Router = require('../router/router')
 
 const app=express()
 app.use(cors({
-  origin: 'http://localhost:5173', // هنا تضيف رابط فرونت اندك أثناء التطوير
+  origin: ' http://localhost:4173', // هنا تضيف رابط فرونت اندك أثناء التطوير
   methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
   credentials: true
 }));
